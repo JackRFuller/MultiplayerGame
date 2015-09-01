bd1968a99664dab78c8c34f5ab3f414c632bed3a
JackRFuller <jackrfuller@outlook.com>
2015-09-01 13:44:37 +0100

    Initial Commit

---
+++ PillarPits/Assets/Scripts/PillarBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarBehaviour : MonoBehaviour {
+
+	[SerializeField] private float minHeight;
+	[SerializeField] private float MaxHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		float Height = Random.Range(minHeight,MaxHeight);
+		transform.localScale = new Vector3(transform.localScale.x, Height, transform.localScale.z);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
