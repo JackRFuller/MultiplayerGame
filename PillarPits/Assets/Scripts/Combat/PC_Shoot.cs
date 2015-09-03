e4dca27320537dce6f60c350899ac44fe0327185
JackRFuller <jackrfuller@outlook.com>
2015-09-03 13:32:50 +0100

    Basic Shooting
    - Basic Shooting Implemented
    - Basic Art style

---
+++ PillarPits/Assets/Scripts/Combat/PC_Shoot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PC_Shoot : MonoBehaviour {
+
+	[Header("Shooting Attributes")]
+	[SerializeField] private float ShotRange;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 fwd = transform.TransformDirection(Vector3.left);
+		RaycastHit hit;
+		Debug.DrawRay(transform.position, fwd * 100, Color.green);
+
+		if(Input.GetMouseButtonDown(0))
+		{
+		
+
+			if(Physics.Raycast(transform.position, fwd, out hit, ShotRange))
+			{
+
+				if(hit.collider.gameObject.name == "Cube")
+				{
+					Debug.Log("Success");
+				}
+			}
+		}
+	
+	}
+}
